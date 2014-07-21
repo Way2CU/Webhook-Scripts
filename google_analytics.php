7284ece1439043142bebdbe7c91fd5d488e32343
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-07-21 15:08:32 +0200

    Initial import

---
+++ google_analytics.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Google Analytics Webhook for Callbox
+ *
+ * Copyright (c) 2014. by Way2CU, http://way2cu.com
+ * Author: Mladen Mijatov
+ *
+ * This script will post single or mutiple events to Google Analytics
+ * through CTM API call.
+ *
+ * This script is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this script. If not, see http://www.gnu.org/licenses/.
+ */
+
+/**
+ * Create authorization header string.
+ *
+ * @param array $config
+ * @return string
+ */
+function makeAuthorization($config) {
+	$auth = base64_encode($config['access_code'].':'.$config['secret_code']);
+	return "Authorization: Basic {$auth}\n";
+}
+
+/**
+ * Send Google Analytics event through CTM API.
+ *
+ * @param array $config
+ * @param array $event
+ * @param integer $call_id
+ * @return boolean
+ */
+function sendEvent($config, $event, $call_id) {
+	$result = false;
+	$content = http_build_query($event);
+	$content_length = strlen($content);
+
+	// prepare header
+	$header = "POST /api/v1/ga/{$call_id}.json HTTP/1.1\n";
+	$header .= "Content-Type: application/x-www-form-urlencoded\n";
+	$header .= "Content-Length: " . strlen($content_length) . "\n";
+	$header .= makeAuthorization($config);
+	$header .= "Connection: close\n\n";
+
+	// open socket
+	$response = null;
+	$socket = fsockopen('ssl://'.$config['endpoint_url'], 443, $error_number, $error_string, 5);
+
+	if ($socket && $error_number == 0) {
+		// send and receive data
+		fputs($socket, $header.$content);
+		$raw_data = stream_get_contents($socket, 1024);
+
+		// parse response
+		$response = json_decode($raw_data);
+
+		// close socket
+		fclose($socket);
+
+		// assume everything went according to plan
+		// TODO: properly parse response
+		$result = true;
+	}
+
+	return $result;
+}
+
+// get call data
+$call_data = json_decode(file_get_contents('php://input'));
+
+// data to send
+$call_id = $call_data['id'];
+$event_data = array(
+	array(
+		'ga_category'	=> '',
+		'ga_action'		=> '',
+		'ga_label'		=> '',
+		'ga_value'		=> '',
+		'uacode'		=> ''
+	)
+);
+
+// access configuration
+$config = array(
+		'endpoint_url'	=> 'api.calltrackingmetrics.com',
+		'access_code' 	=> '',
+		'secret_code'	=> ''
+	);
+
+// post all the events
+foreach ($event_data as $event)
+	sendEvent($config, $event, $call_id);
+
+?>
